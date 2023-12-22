6c37d0f96fbe059cab28e936ab1bc8c85c1ca0b6
Adry1221 <154574191+Adry1221@users.noreply.github.com>
2023-12-22 11:51:17 +0800

    Add files via upload

---
+++ data/Layanan_Kesehatan_4.js
@@ -0,0 +1,12 @@
+var json_Layanan_Kesehatan_4 = {
+"type": "FeatureCollection",
+"name": "Layanan_Kesehatan_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "NAMOBJ": "Posyandu Anggrek", "FCODE": null, "LCODE": "GI0020", "REMARK": "Poliklinik\/Polindes\/Posyandu", "SRS_ID": null, "METADATA": null, "LKALMT": null, "TPLYST": "1" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 116.753778056999977, 0.557080228000018 ] ] } },
+{ "type": "Feature", "properties": { "fid": "2", "NAMOBJ": "Posyandu Mawar Indah", "FCODE": null, "LCODE": "GI0020", "REMARK": "Poliklinik\/Polindes\/Posyandu", "SRS_ID": null, "METADATA": null, "LKALMT": null, "TPLYST": "1" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 116.697176741000021, 0.66646046 ] ] } },
+{ "type": "Feature", "properties": { "fid": "3", "NAMOBJ": "Posyandu Mawar Jingga Muktiutama", "FCODE": null, "LCODE": "GI0020", "REMARK": "Poliklinik\/Polindes\/Posyandu", "SRS_ID": null, "METADATA": null, "LKALMT": null, "TPLYST": "1" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 116.706107877000022, 0.58824447500001 ] ] } },
+{ "type": "Feature", "properties": { "fid": "4", "NAMOBJ": "Posyandu Melan", "FCODE": null, "LCODE": "GI0020", "REMARK": "Poliklinik\/Polindes\/Posyandu", "SRS_ID": null, "METADATA": null, "LKALMT": null, "TPLYST": "1" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 116.644177380999963, 0.670421245 ] ] } },
+{ "type": "Feature", "properties": { "fid": "5", "NAMOBJ": "Posyandu Bogenfil", "FCODE": null, "LCODE": "GI0020", "REMARK": "Poliklinik\/Polindes\/Posyandu", "SRS_ID": null, "METADATA": null, "LKALMT": null, "TPLYST": "1" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 116.691301195000051, 0.636371020000013 ] ] } }
+]
+}
